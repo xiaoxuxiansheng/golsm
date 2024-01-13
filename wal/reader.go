57f6052bd7d53a9b75c8c7c4082bb2e40a82f6c4
weixuxu <weixuxu@didiglobal.com>
2024-01-13 11:49:23 +0800

    完善 wal 注释

--- wal/reader.go
+++ wal/reader.go
@@ -11,13 +11,16 @@ import (
 	"github.com/xiaoxuxiansheng/golsm/memtable"
 )
 
+// wal 文件读取器
 type WALReader struct {
-	file   string
-	src    *os.File
-	reader *bufio.Reader
+	file   string        // 预写日志文件名，是包含了目录在内的绝对路径
+	src    *os.File      // 预写日志文件
+	reader *bufio.Reader // 基于 bufio reader 对日志文件的封装
 }
 
+// 构造器函数.
 func NewWALReader(file string) (*WALReader, error) {
+	// 以只读模式打开 wal 文件，要求目标文件必须存在
 	src, err := os.OpenFile(file, os.O_RDONLY, 0644)
 	if err != nil {
 		return nil, err
@@ -30,20 +33,26 @@ func NewWALReader(file string) (*WALReader, error) {
 	}, nil
 }
 
+// 读取 wal 文件，将所有内容注入到 memtable 中，以实现内存数据的复原
 func (w *WALReader) RestoreToMemtable(memTable memtable.MemTable) error {
+	// 读取 wal 文件全量内容
 	body, err := io.ReadAll(w.reader)
 	if err != nil {
 		return err
 	}
 
+	// 兜底保证文件偏移量被重置到起始位置
 	defer func() {
 		_, _ = w.src.Seek(0, io.SeekStart)
 	}()
 
+	// 将文件中读取到的内容解析成一系列 kv 对
 	kvs, err := w.readAll(bytes.NewReader(body))
 	if err != nil {
 		return err
 	}
+
+	// 将所有 kv 数据注入到 memtable 中
 	for _, kv := range kvs {
 		memTable.Put(kv.Key, kv.Value)
 	}
@@ -51,10 +60,14 @@ func (w *WALReader) RestoreToMemtable(memTable memtable.MemTable) error {
 	return nil
 }
 
+// 将文件中读到的原始内容解析成一系列 kv 对数据
 func (w *WALReader) readAll(reader *bytes.Reader) ([]*memtable.KV, error) {
 	var kvs []*memtable.KV
+	// 循环读取每组 kv 对，直到遇到 eof 错误才终止流程
 	for {
+		// 从 reader 中读取首个 uint64 作为 key 长度
 		keyLen, err := binary.ReadUvarint(reader)
+		// 如果遇到 eof 错误说明文件内容已经读取完毕，终止流程
 		if errors.Is(err, io.EOF) {
 			break
 		}
@@ -62,16 +75,19 @@ func (w *WALReader) readAll(reader *bytes.Reader) ([]*memtable.KV, error) {
 			return nil, err
 		}
 
+		// 从 reader 中读取下一个 uint64 作为 val 长度
 		valLen, err := binary.ReadUvarint(reader)
 		if err != nil {
 			return nil, err
 		}
 
+		// 从 reader 中读取对应于 key 长度的字节数作为 key
 		keyBuf := make([]byte, keyLen)
 		if _, err = io.ReadFull(reader, keyBuf); err != nil {
 			return nil, err
 		}
 
+		// 从 reader 中读取对应于 val 长度的字节数作为 val
 		valBuf := make([]byte, valLen)
 		if _, err = io.ReadFull(reader, valBuf); err != nil {
 			return nil, err
--- wal/writer.go
+++ wal/writer.go
@@ -5,13 +5,16 @@ import (
 	"os"
 )
 
+// 预写日志写入口
 type WALWriter struct {
-	file         string
-	dest         *os.File
-	assistBuffer [30]byte
+	file         string   // 预写日志文件名，是包含了目录在内的绝对路径
+	dest         *os.File // 预写日志文件
+	assistBuffer [30]byte // 辅助转移数据使用的临时缓冲区
 }
 
+// 构造器
 func NewWALWriter(file string) (*WALWriter, error) {
+	// 打开 wal 文件，如果文件不存在则进行创建
 	dest, err := os.OpenFile(file, os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return nil, err
@@ -23,15 +26,18 @@ func NewWALWriter(file string) (*WALWriter, error) {
 	}, nil
 }
 
+// 写入一笔 kv 对到 wal 文件中
 func (w *WALWriter) Write(key, value []byte) error {
-	// key 长度、value 长度、key、value
+	// 首先将key 和 value 长度填充到临时缓冲区 assistBuffer 中
 	n := binary.PutUvarint(w.assistBuffer[0:], uint64(len(key)))
 	n += binary.PutUvarint(w.assistBuffer[n:], uint64(len(value)))
 
+	// 依次将 key 长度、val 长度、key、val 填充到 buf 中
 	var buf []byte
 	buf = append(buf, w.assistBuffer[:n]...)
 	buf = append(buf, key...)
 	buf = append(buf, value...)
+	// 将以上内容写入到 wal 文件中
 	_, err := w.dest.Write(buf)
 	return err
 }
