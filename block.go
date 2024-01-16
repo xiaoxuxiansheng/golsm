1a87b3e3e1512ac3b7a438248e003ed72c52176a
weixuxu <weixuxu@didiglobal.com>
2024-01-16 10:07:48 +0800

    布隆过滤器bugfix&完善注释

--- block.go
+++ block.go
@@ -8,14 +8,16 @@ import (
 	"github.com/xiaoxuxiansheng/golsm/util"
 )
 
+// sst 文件中的数据块，和索引、过滤器为一一对应关系
 type Block struct {
-	conf       *Config       // lsm tree 配置问津
-	buffer     [30]byte      // 临时缓冲区
-	record     *bytes.Buffer // 记录缓冲区
+	conf       *Config       // lsm tree 配置文件
+	buffer     [30]byte      // 用于辅助转移数据的临时缓冲区
+	record     *bytes.Buffer // 用于复制溢写数据的缓冲区
 	entriesCnt int           // kv 对数量
 	prevKey    []byte        // 最晚一笔写入的数据的 key
 }
 
+// 数据块构造器
 func NewBlock(conf *Config) *Block {
 	return &Block{
 		conf:   conf,
@@ -25,40 +27,45 @@ func NewBlock(conf *Config) *Block {
 
 // 追加一组kv对到数据块中
 func (b *Block) Append(key, value []byte) {
+	// 兜底执行：设置 prevKey 为当前写入的 key；累加 entriesCnt 数量
 	defer func() {
 		b.prevKey = append(b.prevKey[:0], key...)
 		b.entriesCnt++
 	}()
 
-	// 共享前缀长度
+	// 获取和之前 key 的共享key前缀长度
 	sharedPrefixLen := util.SharedPrefixLen(b.prevKey, key)
 
-	// 共享键长度、剩余键长度、值长度、剩余键、剩余值
+	// 分别设置共享key长度||剩余key长度||值长度
 	n := binary.PutUvarint(b.buffer[0:], uint64(sharedPrefixLen))
 	n += binary.PutUvarint(b.buffer[n:], uint64(len(key)-sharedPrefixLen))
 	n += binary.PutUvarint(b.buffer[n:], uint64(len(value)))
 
+	// 将 共享key长度||剩余key长度||值长度 写入 record buffer
 	_, _ = b.record.Write(b.buffer[:n])
+	// 将 剩余key || value 写入 record buffer
 	b.record.Write(key[sharedPrefixLen:])
 	b.record.Write(value)
 }
 
+// 获取数据块的大小，单位 byte
 func (b *Block) Size() int {
 	return b.record.Len()
 }
 
-// 把块中的数据溢写到 writer 中
+// 把块中的数据溢写到 dest writer 中
 func (b *Block) FlushTo(dest io.Writer) (uint64, error) {
 	defer b.clear()
 	n, err := dest.Write(b.ToBytes())
 	return uint64(n), err
 }
 
+// 将数据块中的数据转为 byte 数组
 func (b *Block) ToBytes() []byte {
 	return b.record.Bytes()
 }
 
-// 清理快中的数据
+// 清理数据块中的数据
 func (b *Block) clear() {
 	b.entriesCnt = 0
 	b.prevKey = b.prevKey[:0]
--- filter/bloom_filter.go
+++ filter/bloom_filter.go
@@ -6,11 +6,13 @@ import (
 	"github.com/spaolacci/murmur3"
 )
 
+// 布隆过滤器
 type BloomFilter struct {
-	m          int      // bitmap 的长度
-	hashedKeys []uint32 // 一系列 key 的 hash 值
+	m          int      // bitmap 的长度，单位 bit
+	hashedKeys []uint32 // 添加到布隆过滤器的一系列 key 的 hash 值
 }
 
+// 布隆过滤器构造器
 func NewBloomFilter(m int) (*BloomFilter, error) {
 	if m <= 0 {
 		return nil, errors.New("m must be postive")
@@ -20,44 +22,59 @@ func NewBloomFilter(m int) (*BloomFilter, error) {
 	}, nil
 }
 
-// 添加 key 到过滤器
+// 添加一个 key 到布隆过滤器
 func (bf *BloomFilter) Add(key []byte) {
 	bf.hashedKeys = append(bf.hashedKeys, murmur3.Sum32(key))
 }
 
-// 过滤器中是否存在 key
+// 判断过滤器中是否存在 key（注意，可能存在假阳性误判问题）
 func (bf *BloomFilter) Exist(bitmap, key []byte) bool {
+	// 生成 bitmap 时，需要把哈希函数个数 k 的值设置在 bitmap 的最后一个 byte 上
 	if bitmap == nil {
 		bitmap = bf.Hash()
 	}
+	// 获取hash 函数的个数 k
 	k := bitmap[len(bitmap)-1]
 
+	// 第一个基准 hash 函数 h1 = murmur3.Sum32
+	// 第二个基准 hash 函数 h2 = h1 >> 17 | h2 << 15
+	// 之后所有使用的 hash 函数均通过 h1 和 h2 线性无关的组合生成
+	// 第 i 个 hash 函数 gi = h1 + i * h2
+
+	// h1
 	hashedKey := murmur3.Sum32(key)
+	// h2
 	delta := (hashedKey >> 17) | (hashedKey << 15)
 	for i := uint32(0); i < uint32(k); i++ {
-		targetBit := (hashedKey + delta) % uint32(len(bitmap)<<3)
+		// gi = h1 + i * h2
+		targetBit := (hashedKey + i*delta) % uint32(len(bitmap)<<3)
+		// 找到对应的 bit 位，如果值为 1，则继续判断；如果值为 0，则 key 肯定不存在
 		if bitmap[targetBit>>3]&(1<<(targetBit&7)) == 0 {
 			return false
 		}
 	}
 
+	// key 映射的所有 bit 位均为 1，则认为 key 存在（存在误判概率）
 	return true
 }
 
 // 生成过滤器对应的 bitmap. 最后一个 byte 标识 k 的数值
 func (bf *BloomFilter) Hash() []byte {
-	// k: hash 函数个数
+	// k: 根据 m 和 n 推导出最佳 hash 函数个数
 	k := bf.bestK()
-
+	// 获取出一个空的 bitmap，最后一个 byte 位值设置为 k
 	bitmap := bf.bitmap(k)
 
-	// 遍历每个 hash key，对 bitmap 进行标记
-	// h1 = murmur3
-	// h2 = ( h1 >> 17 ) | ( h1 << 15 )
-	// hashk = h1 + k * h2
+	// 第一个基准 hash 函数 h1 = murmur3.Sum32
+	// 第二个基准 hash 函数 h2 = h1 >> 17 | h2 << 15
+	// 之后所有使用的 hash 函数均通过 h1 和 h2 线性无关的组合生成
+	// 第 i 个 hash 函数 gi = h1 + i * h2
 	for _, hashedKey := range bf.hashedKeys {
+		// hashedKey 为 h1
+		// delta 为 h2
 		delta := (hashedKey >> 17) | (hashedKey << 15)
 		for i := uint32(0); i < uint32(k); i++ {
+			// 第 i 个 hash 函数 gi = h1 + i * h2
 			// 需要标记为 1 的 bit 位
 			targetBit := (hashedKey + i*delta) % uint32(len(bitmap)<<3)
 			bitmap[targetBit>>3] |= (1 << (targetBit & 7))
@@ -72,10 +89,12 @@ func (bf *BloomFilter) Reset() {
 	bf.hashedKeys = bf.hashedKeys[:0]
 }
 
+// 获取过滤器中存在的 key 个数
 func (bf *BloomFilter) KeyLen() int {
 	return len(bf.hashedKeys)
 }
 
+// 生成一个空的 bitmap
 func (bf *BloomFilter) bitmap(k uint8) []byte {
 	// bytes = bits / 8 (向上取整)
 	bitmapLen := (bf.m + 7) >> 3
@@ -85,6 +104,7 @@ func (bf *BloomFilter) bitmap(k uint8) []byte {
 	return bitmap
 }
 
+// 根据 m 和 n 推算出最佳的 k
 func (bf *BloomFilter) bestK() uint8 {
 	// k 最佳计算公式：k = ln2 * m / n  m——bitmap 长度 n——key个数
 	k := uint8(69 * bf.m / 100 / len(bf.hashedKeys))
